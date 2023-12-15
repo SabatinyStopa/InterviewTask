af31a8142953f34ddc6bcad7e47145c893c58eda
sabatiny <sabatinystopa@gmail.com>
2023-12-15 20:59:05 -0300

    Adding the game manager

--- Assets/Script/Managers/GameManager.cs
+++ Assets/Script/Managers/GameManager.cs
@@ -10,8 +10,19 @@ namespace InterviewTask.Managers
 
         private IEnumerator Start()
         {
+            fadeImage.enabled = true;
             SoundManager.Instance.PlaySound("Music");
-            yield return null;
+
+            var amount = 1f;
+
+            while (amount > 0)
+            {
+                yield return new WaitForSeconds(0.01f);
+                fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, amount);
+                amount -= 0.01f;
+            }
+
+            fadeImage.enabled = false;
         }
     }
 }
